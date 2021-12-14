d6ab0a32aaaf5f5186b5a8d21c4a350ab5fb54ad
Sinan Kulen <sinan.kulen@gmail.com>
2021-12-15 00:42:31 +0300

    develop commit

--- deneme/ViewController.swift
+++ deneme/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("deneme")
+        print("feature commit")
+        print("hotfix commit")
     }
 
 
